22a3e0a38d0318515a849f540abefb515e53172f
dsasmita <dangs.work@gmail.com>
2020-01-26 21:01:35 +0700

    search function

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,9 +5,14 @@ import RoomDetail from "./RoomThumbnail.js";
 class RoomList extends React.Component {
   render() {
     const { rooms, filterText } = this.props;
+
+    const filteredRooms = rooms.filter(function(room) {
+      return room.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
+    });
+
     return (
       <div className="RoomContainer">
-        {rooms.map(room => (
+        {filteredRooms.map(room => (
           <RoomDetail key={room.name} room={room} />
         ))}
       </div>
